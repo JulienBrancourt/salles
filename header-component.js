824b560131aed2631697a3f69f224c46a003a2cf
JulienBrancourt <brancourt.julien@hotmail.fr>
2025-10-22 14:55:14 +0200

    navbar

--- header-component.js
+++ header-component.js
@@ -1,18 +1,24 @@
-class AppHeader extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = `
-        <div class="progress"></div>
-        <nav>
-            <label class="switch">
-                <input type="checkbox" id="themeToggle">
-                <span></span>
-            </label>
-            <a href="index.html">Liste des salles</a>
-            <a href="annuaire.html">Annuaire</a>
-            <a href="calendrier.html">Calendrier</a>
-        </nav>
-    `;
+class AppHeader extends HTMLElement { //on définit la classe AppHeader, qui pourra utilsée comme une balise HTML personnalisée 
+  connectedCallback() { // méthode de web component appelée par le navigateur quand le composant est inséré dans le DOM
+      this.innerHTML = //this fait référence au composant
+        ` 
+            <div class="progress"></div>
+            <nav>
+                <div class="nav-links">
+                <a href="index.html">Liste des salles</a>
+                <a href="annuaire.html">Annuaire</a>
+                <a href="calendrier.html">Calendrier</a>
+                </div>
+                <div class="theme-switch">
+                <label class="switch">
+                    <input type="checkbox" id="themeToggle">
+                    <span></span>
+                </label>
+                </div>
+            </nav>
+        `;
   }
 }
 
-customElements.define('app-header', AppHeader);
+customElements.define('app-header', AppHeader); //méthode pour enregistrer un nouveau web component, le composant étant une instance de la classe
+
