039ee3c6a007b3de05cecde24524b8fc7e32417e
JulienBrancourt <brancourt.julien@hotmail.fr>
2025-12-17 10:10:25 +0100

    ajout page parcours

--- header-component.js
+++ header-component.js
@@ -1,13 +1,14 @@
 class AppHeader extends HTMLElement { //on définit la classe AppHeader, qui pourra utilsée comme une balise HTML personnalisée 
   connectedCallback() { // méthode de web component appelée par le navigateur quand le composant est inséré dans le DOM
       this.innerHTML = //this fait référence au composant
-        ` 
+        `
             <div class="progress"></div>
             <nav>
                 <div class="nav-links">
                 <a href="index.html">Liste des salles</a>
                 <a href="annuaire.html">Annuaire</a>
                 <a href="calendrier.html">Calendrier</a>
+                <a href="parcours.html">Parcours</a>
                 </div>
                 <div class="theme-switch">
                 <label class="switch">
